5fa00c95710a728a0aa86e1883a2abcf78b38fcc
Robinson Gonzalez <rgonzalezr0322@gmail.com>
2021-10-24 12:08:48 -0500

    Initial Commit

---
+++ ContactsApp/ContactsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactsAppApp.swift
+//  ContactsApp
+//
+//  Created by Robinson Gonzalez on 24/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ContactsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
